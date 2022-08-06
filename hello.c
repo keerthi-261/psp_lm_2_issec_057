0665b69b81eb7307009c4a000090646462ea91db
keerthi-261 <110719329+keerthi-261@users.noreply.github.com>
2022-08-06 17:45:20 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,7 +1,40 @@
 // i will write my code here
 #include<stdio.h>
-#include<conio.h>
 void main()
 {
   printf("hello world");
   }
+#include<stdio.h>
+void main()
+{int a,b,c;
+ printf("enter the value of a,b\n")
+   scanf("%d%d",&a,&b);
+ c= a+b;
+ printf("the sum is %d\n",c);
+ }
+  #include<stdio.h>
+void main()
+{
+    int a,b,add;
+    
+ printf("enter the value of a,b\n");
+   scanf("%d%d",&a,&b);
+ add= a+b;
+ printf("the sum of %d and %d is %d\n",a,b,add);
+}
+#include<stdio.h>
+int add(int c,int d)
+{ 
+    int add =c+ d;
+   return add;
+}
+
+ int main()
+{
+    int a,b,sum;
+    printf("Enter the values of a,b\n");
+    scanf("%d%d",&a,&b);
+    sum=add(a,b);
+    printf("the sum of %d and %d is %d",a,b,sum);
+    
+}
